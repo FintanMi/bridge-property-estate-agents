9693460f6fc6331068ab9d73128f0a2d400b4b5d
FintanMi <>
2023-08-28 14:24:56 +0000

    made index page show listings dynamically

--- bridgeproperty/views.py
+++ bridgeproperty/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from listings.models import Listing
+from realtors.models import Realtor
 
 
 def index(request):
-    return render(request, 'bridgeproperty/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': listings
+    }
+    return render(request, 'bridgeproperty/index.html', context)
 
 
 def about(request):
-    return render(request, 'bridgeproperty/about.html')
+    estate_agent = Realtor.objects.order_by('name')
+    top_seller = Realtor.objects.all().filter(top_seller=True)
+    context = {
+        'estate_agent': estate_agent,
+        'top_seller': top_seller
+    }
+    return render(request, 'bridgeproperty/about.html', context)
--- bridgepropertyestateagent/urls.py
+++ bridgepropertyestateagent/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('bridgeproperty.urls')),
     path('admin/', admin.site.urls),
-    path('listings/', include('listings.urls'))
+    path('listings/', include('listings.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
