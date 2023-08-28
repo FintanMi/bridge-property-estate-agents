67a8461fe37ccf01608530744c81eb48d448a52b
FintanMi <>
2023-08-28 15:59:24 +0000

    created search page

---
+++ listings/choices.py
@@ -0,0 +1,25 @@
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+}
+
+price_choices = {
+    '200000': '\N{euro sign}200,000',
+    '300000': '\N{euro sign}300,000',
+    '400000': '\N{euro sign}400,000',
+    '500000': '\N{euro sign}500,000',
+    '600000': '\N{euro sign}600,000',
+    '700000': '\N{euro sign}700,000',
+    '800000': '\N{euro sign}800,000',
+    '1000000': '\N{euro sign}1M+',
+}
+
+province_choices = {
+    'LEN': 'Leinster',
+    'MUN': 'Munster',
+    'CON': 'Connacht',
+    'ULS': 'Ulster',
+}
