966a9456a3306d06ae5d9c93d884313352d78f26
FintanMi <>
2023-08-28 11:40:50 +0000

    styled admin area

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Listing
 
-# Register your models here.
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'estate_agent',
+        'title',
+        'address',
+        'town',
+        'province',
+        'description',
+        'price',
+        'bedrooms',
+        'bathrooms',
+        'sqm',
+        'photo',
+        'is_published',
+        'list_date',
+    )
+    list_display_links = ('title',)
+    list_filter = ('estate_agent',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'address', 'town', 'province', 'price')
+    list_per_page = 10
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Realtor
 
-# Register your models here.
+@admin.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'photo',
+        'description',
+        'phone',
+        'email',
+        'top_seller',
+    )
+    
