0d7bc9e46034223275b1e7e159b47e046d6f654c
FintanMi <>
2023-08-29 18:42:53 +0000

    Inquiry form checks

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from . models import Contact
+from .models import Contact
 from django.contrib import messages
 
 def contact(request):
@@ -13,10 +13,15 @@ def contact(request):
         user_id = request.POST['user_id']
         estate_agent_email = request.POST['estate_agent_email']
 
+        # check if user has made an inquiry
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                return redirect('/listings/'+listing_id)
+
         contact = Contact(listing=listing, listing_id=listing_id, name=name,
         email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
-
-        messages.success(request, 'Your message has been submitted, a member of staff will get back to you')
 
         return redirect('/listings/'+listing_id)
