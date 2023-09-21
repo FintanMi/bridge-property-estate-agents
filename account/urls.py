ce53e2349592a00582cd76f7937f93e787d03d4d
FintanMi <>
2023-09-21 14:46:51 +0000

    Removed slashes on url, not working when deployed to heroku

--- account/urls.py
+++ account/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
-    path('register/', views.register, name='register'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('staffdashboard/', views.staffdashboard, name='staffdashboard'),
-    path('delete_button/<str:id>/', views.delete_button, name='delete_button'),
+    path('register', views.register, name='register'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('staffdashboard', views.staffdashboard, name='staffdashboard'),
+    path('delete_button/<str:id>', views.delete_button, name='delete_button'),
     path('update_listing_form', views.updateListingForm, name='update_listing_form'),
     path('update_listing/<str:id>/', views.update_listing, name='update_listing'),
 ]
